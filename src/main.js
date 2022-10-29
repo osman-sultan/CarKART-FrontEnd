3affc75000b76406741ef39734bddf02fb5c39fa
ozzy <o.sultan2811@gmail.com>
2022-10-29 12:43:46 -0400

    added bootstrap dependencies

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-import './assets/main.css'
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-const app = createApp(App)
+import "./assets/main.css";
 
-app.use(router)
+const app = createApp(App);
 
-app.mount('#app')
+app.use(router);
+
+app.mount("#app");
