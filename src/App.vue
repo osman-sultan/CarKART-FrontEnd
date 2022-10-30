529293681b7309544cb99f6963727ee0fbb4dbd2
ozzy <o.sultan2811@gmail.com>
2022-10-30 14:38:03 -0400

    testing

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,10 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <HelloWorld msg="Yoyoyo!" />
   <nav>
     <RouterLink to="/">Home</RouterLink>
     <RouterLink to="/about">About</RouterLink>
+    <RouterLink to="/contact">Contact</RouterLink>
   </nav>
   <RouterView />
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,13 @@
 <script setup>
+  import HelloWorld from '../components/HelloWorld.vue'
 </script>
 
 <template>
   <main>
-    OK
+    <HelloWorld msg="Yoyoyo!" />
   </main>
 </template>
+
+
+
+
