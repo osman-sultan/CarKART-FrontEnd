3182d7b90cff427c47b783bff316e10602f91153
Michelle Lai <michelle.lai20@gmail.com>
2022-10-30 19:33:09 -0400

    naur

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import "./assets/main.css";
-import "./assets/CarCards.css";
+import "./assets/CarCard.css";
 
 const app = createApp(App);
 
