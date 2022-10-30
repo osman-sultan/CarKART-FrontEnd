e80c2bab8670238a01bcef699340f2b07422d632
Michelle Lai <michelle.lai20@gmail.com>
2022-10-30 17:32:01 -0400

    formatted CarCards

--- src/components/CarCards.vue
+++ src/components/CarCards.vue
@@ -1,17 +1,69 @@
 <template>
     <div class = "container">
-        <div class="row">
-            <div class="col-3">
-                <img src="images/Car1.jpg" alt="Car 1" class="card-img-top">
+        <div class="row justify-content-md-center g-3">
+            <!-- Card 1 -->
+            <div class="col-12 col-md-6 col-lg-3">
+                <div class="card">
+                    <img src="images/Car1.jpg" class="card-img-top" alt="Car 1">
+                    <div class ="card-body">
+                        <h5 class="card-title">Car 1 Listing</h5>
+                        <p class="card-text">
+                            weeeeee
+                        </p>
+                        <p class="card-text">
+                            <small class="text-muted">nice</small>
+                        </p>
+                        <a href="#" class="btn btn-primary">View Listing</a>
+                    </div>
+                </div>
             </div>
-            <div class="col-3">
-                <img src="images/Car2.jpg" alt="Car 2" class="card-img-top">
+            <!-- Card 2 -->
+            <div class="col-12 col-md-6 col-lg-3">
+                <div class="card">
+                    <img src="images/Car2.jpg" class="card-img-top" alt="Car 2">
+                    <div class ="card-body">
+                        <h5 class="card-title">Car 2 Listing</h5>
+                        <p class="card-text">
+                            weeeeee
+                        </p>
+                        <p class="card-text">
+                            <small class="text-muted">nice</small>
+                        </p>
+                        <a href="#" class="btn btn-primary">View Listing</a>
+                    </div>
+                </div>
             </div>
-            <div class="col-3">
-                <img src="images/Car3.jpg" alt="Car 3" class="card-img-top">
+            <!-- Card 3 -->
+            <div class="col-12 col-md-6 col-lg-3">
+                <div class="card">
+                    <img src="images/Car3.jpg" class="card-img-top" alt="Car 3">
+                    <div class ="card-body">
+                        <h5 class="card-title">Car 3 Listing</h5>
+                        <p class="card-text">
+                            weeeeee
+                        </p>
+                        <p class="card-text">
+                            <small class="text-muted">nice</small>
+                        </p>
+                        <a href="#" class="btn btn-primary">View Listing</a>
+                    </div>
+                </div>
             </div>
-            <div class="col-3">
-                <img src="images/Car4.jpg" alt="Car 4" class="card-img-top">
+            <!-- Card 4 -->
+            <div class="col-12 col-md-6 col-lg-3">
+                <div class="card">
+                    <img src="images/Car4.jpg" class="card-img-top" alt="Car 4">
+                    <div class ="card-body">
+                        <h5 class="card-title">Car 4 Listing</h5>
+                        <p class="card-text">
+                            weeeeee
+                        </p>
+                        <p class="card-text">
+                            <small class="text-muted">nice</small>
+                        </p>
+                        <a href="#" class="btn btn-primary">View Listing</a>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import "./assets/main.css";
+import "./assets/CarCards.css";
 
 const app = createApp(App);
 
