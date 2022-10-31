86cca0c8dc460d23c45eb99a09a7bca45c5ebb6d
Adam S <72041542+AdamS260@users.noreply.github.com>
2022-10-31 08:34:18 -0400

    Added footer to App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,17 @@
 import { RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
 import Header from './components/Header.vue'
-
+import './assets/main.css'
 </script>
 
 <template>
   <Header />
   <HelloWorld msg="Yoyoyo!" />
   <RouterView />
+  <footer class="bg-light">
+      <p>
+          Disclaimer: This is not a professional website
+      </p>
+  </footer>
 </template>
 
