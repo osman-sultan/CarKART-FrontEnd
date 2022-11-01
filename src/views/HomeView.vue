404dcf1f39caf8e1ae6615ba62c9616f68f6cebd
ozzy <o.sultan2811@gmail.com>
2022-11-01 13:07:46 -0400

    did stuff

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,14 @@
 <script setup>
+  import axios from 'axios';
   import HelloWorld from '../components/HelloWorld.vue'
   import CarCardDisplay from '../components/CarCardDisplay.vue'
   import KeywordSearch from '../components/KeywordSearch.vue'
+  import CreateListing from '../components/CreateListing.vue';
 </script>
 
 <template>
   <main>
+    <CreateListing />
     <CarCardDisplay />
   </main>
 </template>
