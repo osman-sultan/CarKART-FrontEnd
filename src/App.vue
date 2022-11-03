13c9a8ed05b616d99f3f786953fe990fd09c7056
Adam S <72041542+AdamS260@users.noreply.github.com>
2022-11-02 22:52:46 -0400

    Removed <HelloWorld> call and import in App

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,11 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 import Header from './components/Header.vue'
 import './assets/main.css'
 </script>
 
 <template>
   <Header />
-  <HelloWorld msg="Yoyoyo!" />
   <RouterView />
   <footer class="bg-light">
       <p>
