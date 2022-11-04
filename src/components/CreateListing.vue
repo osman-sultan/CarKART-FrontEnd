cf81388d0f54ca3ade382c82888a038520962168
ozzy <o.sultan2811@gmail.com>
2022-11-04 14:07:01 -0400

    proper fields for create listing

--- src/components/CreateListing.vue
+++ src/components/CreateListing.vue
@@ -42,7 +42,7 @@ const form = ref([{
         <div class="modal-body">
           <div class="mb-3">
             <label for="exampleFormControlInput1" class="form-label">Car ID</label>
-            <input type="text" class="form-control" id="exampleFormControlInput1" v-model="form.id" placeholder="ID">
+            <input type="number" class="form-control" id="exampleFormControlInput1" v-model="form.id" placeholder="1">
             </div>
             <div class="mb-3">
             <label for="exampleFormControlInput2" class="form-label">Car Make</label>
@@ -54,15 +54,15 @@ const form = ref([{
             </div>
             <div class="mb-3">
             <label for="exampleFormControlInput4" class="form-label">Car Release Year</label>
-            <input type="text" class="form-control" id="exampleFormControlInput4" v-model="form.releaseYear" placeholder="Year">
+            <input type="number" class="form-control" id="exampleFormControlInput4" v-model="form.releaseYear" placeholder="2022">
             </div>
             <div class="mb-3">
             <label for="exampleFormControlInput5" class="form-label">Fuel Type</label>
             <input type="text" class="form-control" id="exampleFormControlInput5" v-model="form.fuelType" placeholder="Fuel Type">
             </div>
             <div class="mb-3">
-            <label for="exampleFormControlInput6" class="form-label">Price</label>
-            <input type="text" class="form-control" id="exampleFormControlInput6" v-model="form.price" placeholder="Enter value $$$">
+            <label for="exampleFormControlInput6" class="form-label">Price ($CAD)</label>
+            <input type="number" class="form-control" id="exampleFormControlInput6" v-model="form.price" placeholder="0">
             </div>
             <div class="mb-3">
             <label for="exampleFormControlInput7" class="form-label">Vehicle Type</label>
@@ -70,11 +70,11 @@ const form = ref([{
             </div>
             <div class="mb-3">
             <label for="exampleFormControlInput8" class="form-label">Horsepower</label>
-            <input type="text" class="form-control" id="exampleFormControlInput8" v-model="form.hp" placeholder="Horsepower">
+            <input type="number" class="form-control" id="exampleFormControlInput8" v-model="form.hp" placeholder="0">
             </div>
             <div class="mb-3">
-            <label for="exampleFormControlInput9" class="form-label">Mileage</label>
-            <input type="text" class="form-control" id="exampleFormControlInput9" v-model="form.mileage" placeholder="Mileage">
+            <label for="exampleFormControlInput9" class="form-label">Mileage (MPG)</label>
+            <input type="number" class="form-control" id="exampleFormControlInput9" v-model="form.mileage" placeholder="0">
             </div>
             <div class="mb-3">
             <label for="exampleFormControlInput10" class="form-label">Colour</label>
@@ -86,7 +86,7 @@ const form = ref([{
             </div>
             <div class="mb-3">
             <label for="exampleFormControlInput11" class="form-label">Image URL</label>
-            <input type="text" class="form-control" id="exampleFormControlInput11" v-model="form.carURL" placeholder="URL">
+            <input type="url" class="form-control" id="exampleFormControlInput12" v-model="form.carURL" placeholder="URL">
             </div>
         </div>
         <div class="modal-footer">
