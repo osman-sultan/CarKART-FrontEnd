2c46aa8275618ed6751e84e8dcc9c52e95ef537d
ozzy <o.sultan2811@gmail.com>
2022-11-10 23:10:29 -0500

    If no cars, id = 1 for new car

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,7 +9,7 @@
   const init = onMounted(async () => {
     await axios
     .get('http://localhost:8085/cars')
-    .then(response => {listOfCars.value = response.data, nextId.value = response.data[Object.keys(response.data).length - 1].id + 1})
+    .then(response => {listOfCars.value = response.data, nextId.value = (Object.keys(response.data).length > 0) ? response.data[Object.keys(response.data).length - 1].id + 1 : 1})
     })
   
     async function deleteCar(id){
