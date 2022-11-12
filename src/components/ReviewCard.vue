5714e4c0bd2110c55025fec34c78ca73bce2ca85
Adam S <72041542+AdamS260@users.noreply.github.com>
2022-11-11 19:34:30 -0500

    Create ReviewCard.vue

---
+++ src/components/ReviewCard.vue
@@ -0,0 +1,20 @@
+<script setup>
+defineProps({
+    review:{
+        type: Object,
+        required: true
+    }
+})
+</script>
+
+<template>
+    <div class="card" style="width: 80%;">
+        <div class="card-body">
+            <h5 class="card-title">{{ review.car.company.make }} {{ review.car.model }}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Car Id: {{review.car.id}}</h6>
+            <p class="card-text">{{review.reviewText}}</p>
+            <button type="button" class="btn btn-secondary">Edit review</button>
+            <button type="button" class="btn btn-danger">Delete review</button>
+        </div>
+    </div>
+</template>
