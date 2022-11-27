b6b14b043decf6563a4555e90691ada39356d521
Osman Sultan <o.sultan2811@gmail.com>
2022-11-27 11:04:22 -0500

    filters

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,11 +7,18 @@
   
   const listOfCars = ref([])
   const filteredCars = ref([])
+  const listOfCompanies = ref([])
   const nextId = ref(0)
   const init = onMounted(async () => {
     await axios
     .get('http://localhost:8085/cars')
-    .then(response => {listOfCars.value = response.data, filteredCars.value = response.data, nextId.value = (Object.keys(response.data).length > 0) ? response.data[Object.keys(response.data).length - 1].id + 1 : 1})
+    .then(response => {listOfCars.value = response.data, 
+      filteredCars.value = response.data, 
+      nextId.value = (Object.keys(response.data).length > 0) ? 
+      response.data[Object.keys(response.data).length - 1].id + 1 : 1})
+    await axios
+    .get('http://localhost:8085/company')
+    .then(reponse => listOfCompanies.value = reponse.data)
   })
   
   async function deleteCar(id){
@@ -31,6 +38,7 @@
     .post('http://localhost:8085/cars', {
       "id": nextId,
       "make": form.make,
+      "sellerId": 1,
       "model": form.model,
       "releaseYear": form.releaseYear,
       "fuelType": form.fuelType,
@@ -57,6 +65,7 @@
     .put('http://localhost:8085/cars/' + form.id, {
       "id": form.id,
       "make": form.make,
+      "sellerId": 1,
       "model": form.model,
       "releaseYear": form.releaseYear,
       "fuelType": form.fuelType,
@@ -88,29 +97,28 @@
   const companyFilter = ref([])
   
   watch(companyFilter, () => {
-    filteredCars.value = listOfCars.value.filter(listOfCars.some(car => car.company.make.includes(companyFilter)))
+    console.log(companyFilter.value.length)
+    if (companyFilter.value.length == 0) {
+      filteredCars.value = listOfCars.value 
+    }
+    else{
+      filteredCars.value = listOfCars.value.filter(car => companyFilter.value.includes(car.company.make))
+    }
   })
 </script>
 
 <template>
   <main>
-    <div>{{ companyFilter }}</div>
-    <div class="form-check m-4" v-for="car in listOfCars">
-      <input class="form-check-input" type="checkbox" :id="car.company.make" :value="car.company.make" v-model="companyFilter">
-      <label class="form-check-label" :for="car.company.make">
-        {{ car.company.make }}
-      </label>
-    </div>
+    
+    
     <div class="container p-4">
       <form class="row row-cols-lg-auto g-3 align-items-center justify-content-center">
         <div class="col-12 d-flex justify-content-center w-75">
-          <label class="visually-hidden" for="inlineFormInputGroupUsername">Username</label>
           <div class="input-group">
             <input type="text" class="form-control" id="inlineFormInputGroupUsername" placeholder="Searching for a listing..." v-model="search">
             <div class="input-group-text"><i class="bi bi-search"></i></div>
           </div>
         </div>
-
         <!-- Button to trigger create modal -->
         <div class="col-12 d-flex justify-content-center">
           <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#createModal">
@@ -119,18 +127,37 @@
         </div>
       </form>
     </div>
+    
+    <div class="container">
+      <div class="row">
+        <div class="col-2 filters">
+          <div class="form-check m-4" v-for="company in listOfCompanies" v-bind:key="company.make">
+            <input class="form-check-input" type="checkbox" :id="company.make" :value="company.make" v-model="companyFilter">
+            <label class="form-check-label" :for="company.make">
+              {{ company.make }}
+            </label>
+          </div>
+        </div>
 
-    <CreateListing 
-    @add-car="addCar"
-    :nextId = "nextId"/>
-    <CarCardDisplay @delete-car="deleteCar" 
-    @update-car="updateCar"  
-    :cars="filteredCars" />
+        <div class="col-10">
+          <CreateListing 
+          @add-car="addCar"
+          :nextId = "nextId"/>
+          <CarCardDisplay @delete-car="deleteCar" 
+          @update-car="updateCar"  
+          :cars="filteredCars" />
+        </div>
+      </div>
+    </div>
   </main>
 </template>
 
 <style>
   @import url("https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css");
+
+  .filters {
+    border-right: 2px solid #D3D3D3;
+  }
 </style>
     
 
