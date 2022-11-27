488bc02dd4470d9871815cc5c7c4faa0a053d0e9
Kevin Kim <kimkevin0612@gmail.com>
2022-11-27 16:03:56 -0500

    Implemented Login and Registration pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import LoginView from "../views/LoginView.vue";
 import ListingView from "../views/ListingView.vue";
+import RegisterView from "../views/RegisterView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -35,6 +37,18 @@ const router = createRouter({
       path: "/compare",
       name: "compare",
       component: () => import("../views/CompareView.vue"),
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: LoginView,
+      props: true,
+    },
+    {
+      path: "/register",
+      name: "register",
+      component: RegisterView,
+      props: true,
     }
   ],
 });
