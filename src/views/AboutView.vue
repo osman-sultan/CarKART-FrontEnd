097273b1e1193121dbadefa914c79b2b6a1c5bc2
Michael Iaccino <michaeliaccino@mail.utoronto.ca>
2022-11-27 14:57:21 -0500

    Added relevant car sites & website creation site

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -7,7 +7,7 @@
       <p>CARKart is an online marketplace that connects car buyers and sellers. Sellers can browse through car listings through functionalities including creating or viewing car listings, using search filters, sharing or viewing customer reviews, and generating car comparisons. The target audience are adults ages 18 and above. On average, buyers only examine and compare approximately 3.3 car brands when shopping [1]. Thus, it shows a lack of market awareness of buyers which limits their purchasing options. This web application aims to help buyers gain autonomy in the buying process by providing information including pricing, pictures, comparisons and reviews to help them make an informed purchasing decision.</p>
 
     <h3>About the Team</h3>
-      <p>This is an undergraduate term project</p>
+      <p>This is an undergraduate term project for MIE350. Our team consists of 7 members studying Industrial Engineering at the University of Toronto</p>
      
     <div class="card-group">
 
@@ -87,13 +87,44 @@
         <h5 class="card-title text-center">Adam Stechishin</h5>
         </div>
       </div>
-
       </div>
+      <p>
+      </p>
 
 
     <h3>Relevant Websites</h3>
       <p>A reasonable collection of links, effectively organized and identified to help site visitors find the sites of key other content sites (e.g., businesses, other communities) that are directly relevant</p>
 
+      <p>Car Companies:</p>
+      <ul class="list-group">
+        <li class="list-group-item" >
+          <a href="https://www.bmw.ca/en/home.html">BMW</a> - Bayerische Motoren Werke AG, is a German multinational manufacturer of performance luxury vehicles and motorcycles headquartered in Munich, Bavaria, Germany.
+        </li>
+        <li class="list-group-item">
+          <a href="https://www.seat.com/">Seat</a> - SEAT S.A. is a Spanish car manufacturer, which sells its vehicles under the SEAT and Cupra brands.
+        </li>
+        <li class="list-group-item">
+          <a href="https://www.vw.ca/en.html">Volkswagen</a> - Volkswagen, is a German motor vehicle manufacturer headquartered in Wolfsburg, Lower Saxony, Germany.
+        </li>
+      </ul>
+      <p>
+      </p>
+
+
+      <p>Website made using:</p>
+      <ul class="list-group">
+        <li class="list-group-item" >
+          <a href="https://getbootstrap.com/">Bootstrap</a>
+        </li>
+        <li class="list-group-item">
+          <a href="https://vuejs.org/">Vue</a>
+        </li>
+        <li class="list-group-item">
+          <a href="https://vuejs.org/">A third item</a>
+        </li>
+      </ul>
+
+        
   </div>
 </template>
 
