29c1273e17c590476d20add9ec8e14fa99537fc5
ozzy <o.sultan2811@gmail.com>
2022-12-04 14:45:59 -0500

    made about page responsive

--- src/App.vue
+++ src/App.vue
@@ -28,11 +28,16 @@
   <div v-else>
     <Header :userId="userId" @logout="Logout"/>
     <RouterView />
-    <footer class="bg-light mt-5">
-      <p class="footer">
-        MIE350 Team 7
-        <br>Disclaimer: This is not a professional website.
-      </p>
+    <footer class="p-5 bg-light text-center position-relative">
+      <div class="container">
+        <p class="footer">
+          MIE350 Team 7
+          <br>Disclaimer: This is not a professional website.
+        </p>
+        <a href="#" class="position-absolute bottom-0 end-0 p-5">
+          <i class="bi bi-arrow-up-circle h1"></i>
+        </a>
+      </div>
     </footer>
   </div>
 </template>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,101 +1,207 @@
 <template>
-  <div class="about container">
-    <p></p>
-    <h3>Purpose</h3>
-      <p>According to Kantar’s 2017 Auto Shopper Study, approximately one in two car buyers commence their search for purchasing vehicles using online resources [1]. Thus, there is great potential for the user journey to influence consumer behaviour.</p>
-
-    <h3>About CarKART</h3>
-      <p>CARKart is an online marketplace that connects car buyers and sellers. Sellers can browse through car listings through functionalities including creating or viewing car listings, using search filters, sharing or viewing customer reviews, and generating car comparisons. The target audience are adults ages 18 and above. On average, buyers only examine and compare approximately 3.3 car brands when shopping [1]. Thus, it shows a lack of market awareness of buyers which limits their purchasing options. This web application aims to help buyers gain autonomy in the buying process by providing information including pricing, pictures, comparisons and reviews to help them make an informed purchasing decision.</p>
-
-    <h3>About the Team</h3>
-      <p>This is an undergraduate term project for MIE350. Our team consists of 7 members studying Industrial Engineering at the University of Toronto.</p>
-     
-    <div class="card-group">
-
-      <div class="card w-25">
-                    <img 
-                    src="https://media-exp1.licdn.com/dms/image/C5603AQEtsDKD9w2dvA/profile-displayphoto-shrink_200_200/0/1660511763359?e=1674086400&v=beta&t=hdejQIYc-YaKu2yuF23Ms9oG_0T-1ckmcnzj4DOiKJk" 
-                    alt="Image of Student" 
-                    width="10" height="10"
-                    class="card-img-top">
-          <div class="card-body">
-        <h5 class="card-title text-center">Kevin Kim <a href="https://www.linkedin.com/in/kevinsunghookim/"><i class="bi bi-linkedin"></i></a></h5>
-        </div>
-      </div>
-
-      <div class="card w-25">
-                    <img 
-                    src="https://media-exp1.licdn.com/dms/image/D5603AQEBi5aQduS1SA/profile-displayphoto-shrink_200_200/0/1666883890942?e=1674086400&v=beta&t=HWRCtW7JksY6ER6q7crfxKi_lGj5V7xZO0qotUSNBzA" 
-                    alt="Image of Student" 
-                    width="10" height="10"
-                    class="card-img-top">
-          <div class="card-body">
-        <h5 class="card-title text-center">Vanathi Anpalagan <a href="https://www.linkedin.com/in/vanathianpalagan/"><i class="bi bi-linkedin"></i></a></h5>
-        </div>
-      </div>
-
-      <div class="card w-25">
-                    <img 
-                    src="https://media-exp1.licdn.com/dms/image/C4E03AQHTWtY2VGjFBA/profile-displayphoto-shrink_200_200/0/1643406681732?e=1674086400&v=beta&t=Wg-6QjDeqmPUlkeNDEzSZ2LWZropTH0BsQ0AJZNTavg" 
-                    alt="Image of Student" 
-                    width="10" height="10"
-                    class="card-img-top">
-          <div class="card-body">
-        <h5 class="card-title text-center">Osman Sultan <a href="https://www.linkedin.com/in/osmansultan-/"><i class="bi bi-linkedin"></i></a></h5>
-        </div>
-      </div>
-
-      <div class="card w-25">
-                    <img 
-                    src="https://media-exp1.licdn.com/dms/image/D4D03AQHFiqNkTuVIHw/profile-displayphoto-shrink_200_200/0/1664584756386?e=1674086400&v=beta&t=1p7vaZ0B7f4pTOnHMKkpz8q_qYmxnkU8kt99AHQ7HDw" 
-                    alt="Image of Student" 
-                    width="10" height="10"
-                    class="card-img-top">
-          <div class="card-body">
-        <h5 class="card-title text-center">Michael Iaccino <a href="https://www.linkedin.com/in/michaeliaccino/"><i class="bi bi-linkedin"></i></a></h5>
-        </div>
-      </div>
-
-      <div class="card w-25">
-                    <img 
-                    src="https://media-exp1.licdn.com/dms/image/C4E03AQE1J8MgkFN-rg/profile-displayphoto-shrink_200_200/0/1652326403470?e=1674086400&v=beta&t=biRjkJ4guMokWUkqDvD1VAfdK2obRkstauaumhY__RY" 
-                    alt="Image of Student" 
-                    width="10" height="10"
-                    class="card-img-top">
-          <div class="card-body">
-        <h5 class="card-title text-center">Michelle Lai <a href="https://www.linkedin.com/in/michellemxlai/"><i class="bi bi-linkedin"></i></a></h5>
-        </div>
-      </div>
-
-      <div class="card w-25">
-                    <img 
-                    src="https://media-exp1.licdn.com/dms/image/D5603AQEqFusyvc1FQw/profile-displayphoto-shrink_200_200/0/1665704923416?e=1674086400&v=beta&t=sf1UpfDQFQh8x4EGYJAu-RB_rZbLGaP0JSHjd9UqkiM" 
-                    alt="Image of Student" 
-                    width="10" height="10"
-                    class="card-img-top">
-          <div class="card-body">
-        <h5 class="card-title text-center">Anton Mlynczyk <a href="https://www.linkedin.com/in/antonmlynczyk/"><i class="bi bi-linkedin"></i></a></h5>
-        </div>
-      </div>
-
-      <div class="card w-25">
-                    <img 
-                    src="https://media-exp1.licdn.com/dms/image/D5603AQH6uJ1JaqMLMg/profile-displayphoto-shrink_200_200/0/1664499640825?e=1674086400&v=beta&t=nZvqZ92C_AwmmM9RB4MmQnahJGWCwt2-r4GLr0xZFQo" 
-                    alt="Image of Student" 
-                    width="10" height="10"
-                    class="card-img-top">
-          <div class="card-body">
-        <h5 class="card-title text-center">Adam Stechishin <a href="https://www.linkedin.com/in/adam-stechishin-986329211/"><i class="bi bi-linkedin"></i></a></h5>
-        </div>
-      </div>
-      </div>
+  <section id="purpose" class="px-5 py-2">
+    <div class="container">
+      <h3>Purpose</h3>
       <p>
+        According to Kantar’s 2017 Auto Shopper Study, approximately one in two car 
+        buyers commence their search for purchasing vehicles using online resources [1]. 
+        Thus, there is great potential for the user journey to influence consumer behaviour.
       </p>
+    </div>
+  </section>
+  
+  <section id="about" class="px-5 py-2">
+    <div class="container">
+      <h3>About CarKART</h3>
+      <p>
+        CARKart is an online marketplace that connects car buyers and sellers. 
+        Sellers can browse through car listings through functionalities including 
+        creating or viewing car listings, using search filters, sharing or viewing 
+        customer reviews, and generating car comparisons. The target audience are adults 
+        ages 18 and above. On average, buyers only examine and compare approximately 3.3 
+        car brands when shopping [1]. Thus, it shows a lack of market awareness of buyers 
+        which limits their purchasing options. This web application aims to help buyers 
+        gain autonomy in the buying process by providing information including pricing, 
+        pictures, comparisons and reviews to help them make an informed purchasing decision.
+      </p>
+    </div>
+  </section>
+    
+  <section id="members" class="px-5 py-2">
+    <div class="container">
+      <h3>About the Team</h3>
+      <p>
+        This is an undergraduate term project for MIE350. 
+        Our team consists of 7 members studying Industrial Engineering at the University of Toronto.
+      </p>
+      <div class="row g-4 justify-content-center">
 
+        <div class="col-md-6 col-lg-3">
+          <div class="card h-100">
+            <div class="card-body text-center">
+              <img 
+                src="https://media-exp1.licdn.com/dms/image/C5603AQEtsDKD9w2dvA/profile-displayphoto-shrink_200_200/0/1660511763359?e=1674086400&v=beta&t=hdejQIYc-YaKu2yuF23Ms9oG_0T-1ckmcnzj4DOiKJk" 
+                alt="Image of Sunghoo" 
+                class="rounded-circle mb-3 img-fluid"
+              />
+              <h3 class="card-title mb-3">Sunghoo (Kevin) Kim</h3>
+              <h6 class="card-subtitle mb-2 text-muted">Team Lead <br>Back-End Developer</h6>
+              <p class="card-text">
+                Hey guys! My favourite car is a Nissan Cube :)
+              </p>
+            </div>
+            <div class="card-footer text-center">
+              <a href="https://www.linkedin.com/in/kevinsunghookim/">
+                <i class="bi bi-linkedin text-dark mx-1"></i>
+              </a>
+            </div>
+          </div>
+        </div>
 
-    <h3>Relevant Websites</h3>
-      <p>A reasonable collection of links, effectively organized and identified to help site visitors find the sites of key other content sites (e.g., businesses, other communities) that are directly relevant.</p>
+        <div class="col-md-6 col-lg-3">
+          <div class="card h-100">
+            <div class="card-body text-center">
+              <img 
+                src="https://media-exp1.licdn.com/dms/image/D5603AQEBi5aQduS1SA/profile-displayphoto-shrink_200_200/0/1666883890942?e=1674086400&v=beta&t=HWRCtW7JksY6ER6q7crfxKi_lGj5V7xZO0qotUSNBzA" 
+                alt="Image of Vanathi" 
+                class="rounded-circle mb-3 img-fluid"
+              />
+              <h3 class="card-title mb-3">Vanathi Anpalagan</h3>
+              <h6 class="card-subtitle mb-2 text-muted">UI/UX Designer <br>Front-End Developer</h6>
+              <p class="card-text">
+                Hey guys! My favourite car is a Nissan Cube :)
+              </p>
+            </div>
+            <div class="card-footer text-center">
+              <a href="https://www.linkedin.com/in/vanathianpalagan/">
+                <i class="bi bi-linkedin text-dark mx-1"></i>
+              </a>
+            </div>
+          </div>
+        </div>
 
+        <div class="col-md-6 col-lg-3">
+          <div class="card h-100">
+            <div class="card-body text-center">
+              <img 
+                src="https://media-exp1.licdn.com/dms/image/C4E03AQHTWtY2VGjFBA/profile-displayphoto-shrink_200_200/0/1643406681732?e=1674086400&v=beta&t=Wg-6QjDeqmPUlkeNDEzSZ2LWZropTH0BsQ0AJZNTavg" 
+                alt="Image of Osman" 
+                class="rounded-circle mb-3 img-fluid"
+              />
+              <h3 class="card-title mb-3">Osman Sultan</h3>
+              <h6 class="card-subtitle mb-2 text-muted">Project Manager <br>Full-Stack Lead</h6>
+              <p class="card-text">
+                Hey guys! My favourite car is a Nissan Cube :)
+              </p>
+            </div>
+            <div class="card-footer text-center">
+              <a href="https://www.linkedin.com/in/osmansultan-/">
+                <i class="bi bi-linkedin text-dark mx-1"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-6 col-lg-3">
+          <div class="card h-100">
+            <div class="card-body text-center">
+              <img 
+                src="https://media-exp1.licdn.com/dms/image/D5603AQEqFusyvc1FQw/profile-displayphoto-shrink_200_200/0/1665704923416?e=1674086400&v=beta&t=sf1UpfDQFQh8x4EGYJAu-RB_rZbLGaP0JSHjd9UqkiM" 
+                alt="Image of Anton" 
+                class="rounded-circle mb-3 img-fluid"
+              />
+              <h3 class="card-title mb-3">Anton Mylnczyk</h3>
+              <h6 class="card-subtitle mb-2 text-muted">Back-End Lead</h6>
+              <p class="card-text">
+                Hey guys! My favourite car is a Nissan Cube :)
+              </p>
+            </div>
+            <div class="card-footer text-center">
+              <a href="https://www.linkedin.com/in/antonmlynczyk/">
+                <i class="bi bi-linkedin text-dark mx-1"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-6 col-lg-3">
+          <div class="card h-100">
+            <div class="card-body text-center">
+              <img 
+                src="https://media-exp1.licdn.com/dms/image/D4D03AQHFiqNkTuVIHw/profile-displayphoto-shrink_200_200/0/1664584756386?e=1674086400&v=beta&t=1p7vaZ0B7f4pTOnHMKkpz8q_qYmxnkU8kt99AHQ7HDw" 
+                alt="Image of Michael" 
+                class="rounded-circle mb-3 img-fluid"
+              />
+              <h3 class="card-title mb-3">Michael Iaccino</h3>
+              <h6 class="card-subtitle mb-2 text-muted">Front-End Developer</h6>
+              <p class="card-text">
+                Hey guys! My favourite car is a Nissan Cube :)
+              </p>
+            </div>
+            <div class="card-footer text-center">
+              <a href="https://www.linkedin.com/in/michaeliaccino/">
+                <i class="bi bi-linkedin text-dark mx-1"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-6 col-lg-3">
+          <div class="card h-100">
+            <div class="card-body text-center">
+              <img 
+                src="https://media-exp1.licdn.com/dms/image/C4E03AQE1J8MgkFN-rg/profile-displayphoto-shrink_200_200/0/1652326403470?e=1674086400&v=beta&t=biRjkJ4guMokWUkqDvD1VAfdK2obRkstauaumhY__RY" 
+                alt="Image of Michelle" 
+                class="rounded-circle mb-3 img-fluid"
+              />
+              <h3 class="card-title mb-3">Michelle Lai</h3>
+              <h6 class="card-subtitle mb-2 text-muted">UI/UX Lead<br>Front-End Developer</h6>
+              <p class="card-text">
+                Hey guys! My favourite car is a Nissan Cube :)
+              </p>
+            </div>
+            <div class="card-footer text-center">
+              <a href="https://www.linkedin.com/in/michellemxlai/">
+                <i class="bi bi-linkedin text-dark mx-1"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-6 col-lg-3">
+          <div class="card h-100">
+            <div class="card-body text-center">
+              <img 
+                src="https://media-exp1.licdn.com/dms/image/D5603AQH6uJ1JaqMLMg/profile-displayphoto-shrink_200_200/0/1664499640825?e=1674086400&v=beta&t=nZvqZ92C_AwmmM9RB4MmQnahJGWCwt2-r4GLr0xZFQo" 
+                alt="Image of Adam" 
+                class="rounded-circle mb-3 img-fluid"
+              />
+              <h3 class="card-title mb-3">Adam Stechishin</h3>
+              <h6 class="card-subtitle mb-2 text-muted">Front-End Lead</h6>
+              <p class="card-text">
+                Hey guys! My favourite car is a Nissan Cube :)
+              </p>
+            </div>
+            <div class="card-footer text-center">
+              <a href="https://www.linkedin.com/in/adam-stechishin-986329211/">
+                <i class="bi bi-linkedin text-dark mx-1"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </section>
+
+  <section id="relevnt-websites" class="px-5 py-4">
+    <div class="container">
+      <h3>Relevant Websites</h3>
+      <p>
+        A reasonable collection of links, effectively organized 
+        and identified to help site visitors 
+        find the sites of key other content sites 
+        (e.g., businesses, other communities) that are directly relevant.</p>
       <p>Car Companies:</p>
       <ul class="list-group">
         <li class="list-group-item" >
@@ -150,30 +256,27 @@
           <a href="https://www.hyundaicanada.com/">Hyundai</a> - Founded in the year 1967, the Hyundai Motor Company is located in South Korea
         </li>
       </ul>
-      <p>
-      </p>
-
-
-      <p>Website made using:</p>
+      <p class="pt-3">Website made using:</p>
       <ul class="list-group">
-        <li class="list-group-item" >
-          <a href="https://getbootstrap.com/">Bootstrap</a> , 
-          <a href="https://vuejs.org/">Vue</a> , [2]
-          <a href="https://www.kaggle.com/datasets/ander289386/cars-germany">Kaggle Car Data</a>
+        <li class="list-group-item">
+          <i class="bi bi-bootstrap"></i> <a href="https://getbootstrap.com/">Bootstrap</a> , 
+          <a href="https://vuejs.org/">Vue</a> ,
+          <a href="https://spring.io/projects/spring-boot">Spring Boot</a> ,
+          [2] <a href="https://www.kaggle.com/datasets/ander289386/cars-germany">Kaggle Car Data</a>
         </li>
       </ul>
-      <p></p>
-
+    </div>
+  </section>
+    
+  <section id="references" class="px-5 py-2">
+    <div class="container">
       <h3>References</h3>
       <p>[1] Kantar TNS and Google, “THE DRIVE TO DECIDE,” Gearshift Auto Shopper Study 2017, 2017. [Accessed: 12-Oct-2022].  </p>
       <p>[2] Ander, “Germany cars dataset,” Kaggle, 09-Jun-2021. [Online]. Available: https://www.kaggle.com/datasets/ander289386/cars-germany. [Accessed: 12-Oct-2022]. </p>
-        
-  </div>
+    </div>
+  </section> 
+   
 </template>
-
-<style>
-
-</style>
 
 
  
